e77719e83e2ca839b19c032d7fd4179b746c9fe2
Thomas Blommel <tblommel@umich.edu>
2020-05-27 21:40:33 -0400

    Jenkins

--- tests/hubb-test.cpp
+++ tests/hubb-test.cpp
@@ -35,6 +35,7 @@ TEST_CASE("Hubbard Chain Self-Energy Computations")
     Hubb::Ham_MF(t,G,Ut,h0,hmf);
     Hubb::Sigma_2B(t, G, Ut, Sig);
   }
+  REQUIRE(false);
 
   ZColVectorMap hmfVec = ZColVectorMap(hmf.ptr(-1), (Nt+2)*Nsites*Nsites);
   ZColVectorMap hmfcheckVec = ZColVectorMap(hmfcheck.ptr(-1), (Nt+2)*Nsites*Nsites);
