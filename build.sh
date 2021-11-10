c5785ab5faef8ce0a50771dd1acf81eb45d71048
Thomas <tblommel@umich.edu>
2021-11-10 11:51:15 -0500

    remove hard coded directory path

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ cmake                               \
   -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -O3 -g -march=znver2"  \
   -DINCLUDE_DIR=~/Libraries/Install/include/  \
   -DLINK_DIR=~/Libraries/Install/lib  \
+  -DDATA_DIR=/pauli-storage/tblommel/NEdyson_data/ \
+  -DTIMING_DATA_DIR=/pauli-storage/tblommel/NEdyson_data/timing_data/ \
 ..
