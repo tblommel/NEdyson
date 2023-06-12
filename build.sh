c3c8917335b380f9e664af5603c1007fa18a5053
tblommel <50418898+tblommel@users.noreply.github.com>
2023-06-12 18:43:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cmake                               \
   -DCMAKE_BUILD_TYPE=Release        \
   -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -O3 -std=c++11"  \
   -DINCLUDE_DIR=/home/tblommel/Libraries/Install/include/  \
-  -DLINK_DIR=/home/tblommel/Libraries/Install/lib  \
+  -DLINK_DIR=/home/tblommel/Libraries/Install/lib/  \
 ..
